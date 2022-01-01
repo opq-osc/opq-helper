92277f5737d5a7bdf6ccbfcb2254a93de3b02579
fz6m <59400654+fz6m@users.noreply.github.com>
2022-01-02 05:44:21 +0800

    feat: add opq philosophy 🍉

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -70,6 +70,7 @@ module.exports = {
         text: 'Other',
         items: [
           { text: '相关站点', link: '/other/site' },
+          { text: 'OPQ 哲学', link: '/other/philosophy' },
           // { text: '相关站点', link: '/opq-wiki/安装指南' },
         ],
       },
---
+++ docs/other/philosophy.md
@@ -0,0 +1,7 @@
+# OPQ哲学
+
+通过涉猎 OPQ 哲学获得不一样的收获
+
+## [OPQtaverse](https://github.com/opq-osc/OPQtaverse)
+
+OPQ 元宇宙
